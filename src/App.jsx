60290cdd59eecb3062f2bba270648c650f089946
Henok-Al <henok.gebresenbet@gmail.com>
2024-08-09 10:33:03 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@material-tailwind/react";
+
+function App() {
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen">
+        <Button>Click</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
