40d769ed0a012cf5537f9f267c7470ebc5114f2c
Henok-Al <henok.gebresenbet@gmail.com>
2024-08-11 17:55:29 +0300

    search blog

--- src/components/BlogPostCard.jsx
+++ src/components/BlogPostCard.jsx
@@ -1,7 +1,8 @@
 import { Button } from "@material-tailwind/react";
-import React, { useContext } from "react";
+import { useContext } from "react";
 import myContext from "../context/data/myContext";
 import { useNavigate } from "react-router";
+import { Link } from "react-router-dom";
 
 function BlogPostCard() {
   const context = useContext(myContext);
@@ -99,17 +100,20 @@ function BlogPostCard() {
           </div>
 
           {/* See More Button  */}
+          {/* See More Button  */}
           <div className="flex justify-center my-5">
-            <Button
-              style={{
-                background:
-                  mode === "dark" ? "rgb(226, 232, 240)" : "rgb(30, 41, 59)",
-                color:
-                  mode === "dark" ? "rgb(30, 41, 59)" : "rgb(226, 232, 240)",
-              }}
-            >
-              See More
-            </Button>
+            <Link to={"/allblogs"}>
+              <Button
+                style={{
+                  background:
+                    mode === "dark" ? "rgb(226, 232, 240)" : "rgb(30, 41, 59)",
+                  color:
+                    mode === "dark" ? "rgb(30, 41, 59)" : "rgb(226, 232, 240)",
+                }}
+              >
+                See More
+              </Button>
+            </Link>
           </div>
         </div>
       </section>
